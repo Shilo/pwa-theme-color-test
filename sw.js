0a55cd6e0d59dd4c03f954aaa61e80faccf4316f
Shilo <shilo86@gmail.com>
2026-03-03 02:27:47 -0800

    feat: Implement theme toggling with dynamic theme color updates and increment service worker cache version.

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Minimal service worker to pass PWA install criteria
 self.addEventListener('install', (e) => {
     e.waitUntil(
-        caches.open('pwa-theme-color-store-v2').then((cache) => cache.addAll([
+        caches.open('pwa-theme-color-store-v3').then((cache) => cache.addAll([
             './',
             'index.html',
             'manifest.json',
